0154d1b7620981f616f50fd69016922e22a0c626
Carlos André Ferrari <caferrari@gmail.com>
2010-02-20 12:35:20 -0300

    Now python class is webservice fail safe

--- python/QueyClient.py
+++ python/QueyClient.py
@@ -1,4 +1,6 @@
-import sys, tempfile, urllib, os, hashlib
+#!/usr/bin/python2.6
+# -*- coding: utf-8 -*-#
+import sys, tempfile, urllib, os, hashlib, time
 from json import JSONDecoder
 
 class QueryClient:
@@ -22,20 +24,42 @@ class QueryClient:
 	
 	def loadUrl(self, url):
 		cache = self.cache_dir + hashlib.md5(url).hexdigest()
-		if os.path.isfile(cache):
-			content = open(cache).read()
-			return JSONDecoder().decode(content)			
 		
-		res = urllib.urlopen(url)
-		if (res.getcode() == 200):
-			content = res.read()
+		if os.path.isfile(cache):
+			# Se o tempo de vida do cache estiver aceitavel retorna os dados
+			age = time.time() - os.stat(cache).st_mtime
+			if age <= 300:
+				content = open(cache).read()
+				return JSONDecoder().decode(content)
+			
+			# Senão define o horario de criação para agora, para impedir multiplas tentativas de criação do cache
+			# e cria o cache novamente
+			else:
+				os.utime(cache, None)
+		
+		# Tanta carregar um novo recordset
+		try:
+			res = urllib.urlopen(url)
+			# se o webservice retornou o codigo de sucesso, cria um novo cache e retorna a informação!
+			if (res.getcode() == 200):
+				content = res.read()
  
-			file = open(cache, 'w')
-			file.write(content)
-			file.close();
+				file = open(cache, 'w')
+				file.write(content)
+				file.close();
 			
+				return JSONDecoder().decode(content)
+		finally:
+			pass
+		
+		# Caso a requisição falhou, muda a data de criacao do cache para agora, e este será o atual
+		# até dar o timeout e uma nova tentativa no webservice
+		if os.path.isfile(cache):	
+			os.utime(cache, None)
+			content = open(cache).read()
 			return JSONDecoder().decode(content)
-			
+		
+		# Se o cache nao existir e a requisição falhar, uma exception é lançada	
 		raise Exception('Erro ao obter arquivo')
 
 	def query(self, acao, pars=''):
